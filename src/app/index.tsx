d07a2077fe9c573446062f6e922de39acd0c6329
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2023-02-05 16:41:47 +0530

    Added Redux integration

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from './store/store'
 import App from './App'
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
