50aafc170a6c2e26c77004d07813d20543b3fe60
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2023-02-21 00:42:12 +0530

    Changed BrowserRouter to HashRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.scss'
 import Connection from './pages/Connection'
 
