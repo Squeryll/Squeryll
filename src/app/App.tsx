a21bacc4931597a1981c175cd97450ddeee58669
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2023-02-21 00:19:47 +0530

    Added react-router-dom in the app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,16 @@
 import { FC } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.scss'
 import Connection from './pages/Connection'
 
 const App: FC = () => {
-  return <Connection />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Connection />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
